ed4d87e5ce53550c54c64f7891da2882159a5653
jonathanzASC6 <jzurita456@gmail.com>
2019-07-16 12:05:19 -0400

    work on this

---
+++ sketch.js
@@ -0,0 +1,21 @@
+// *Click One Box project
+function setup(){
+    createCanvas(600, 600);
+    background(0, 255, 0);
+}
+let valueR = 51;
+let valueG = 51;
+let valueB = 225;
+function draw(){
+    rectMode(RADIUS);
+    fill(valueR, valueG, valueB);
+    rect(600, 600, 600, 600);
+}
+
+function mouseClicked(){
+
+}
+
+
+
+
